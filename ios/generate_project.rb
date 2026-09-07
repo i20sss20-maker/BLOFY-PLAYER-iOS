21c0b6a41dc465c5b7439fb983b6b8cd6c054b61
i20sss20-maker <i20sss20@gmail.com>
2026-09-07 23:28:48 +0300

    Link official VLCKit XCFramework

--- ios/generate_project.rb
+++ ios/generate_project.rb
@@ -6,9 +6,18 @@ project=Xcodeproj::Project.new('BlofyPlayer.xcodeproj')
 target=project.new_target(:application,'BlofyPlayer',:ios,'16.0')
 sources=project.main_group.new_group('Sources')
 Dir.glob('BlofyPlayer/*.{swift,m,h}').sort.each{|p| r=sources.new_file(p); target.source_build_phase.add_file_reference(r) unless p.end_with?('.h')}
+frameworks=project.main_group.new_group('Frameworks')
+vlc_path='Vendor/VLCKit.xcframework'
+raise 'Missing VLCKit.xcframework' unless File.directory?(vlc_path)
+vlc_ref=frameworks.new_file(vlc_path)
+target.frameworks_build_phase.add_file_reference(vlc_ref, true)
+embed=target.new_copy_files_build_phase('Embed Frameworks')
+embed.dst_subfolder_spec='10'
+embed_file=embed.add_file_reference(vlc_ref, true)
+embed_file.settings={'ATTRIBUTES'=>['CodeSignOnCopy','RemoveHeadersOnCopy']}
 target.build_configurations.each do |c|
- c.build_settings.merge!({'PRODUCT_BUNDLE_IDENTIFIER'=>'tv.blofy.player.ios','PRODUCT_NAME'=>'BlofyPlayer','SWIFT_VERSION'=>'5.0','IPHONEOS_DEPLOYMENT_TARGET'=>'16.0','TARGETED_DEVICE_FAMILY'=>'1,2','INFOPLIST_FILE'=>'BlofyPlayer/Info.plist','GENERATE_INFOPLIST_FILE'=>'NO','CODE_SIGNING_ALLOWED'=>'NO','CODE_SIGNING_REQUIRED'=>'NO','CODE_SIGN_IDENTITY'=>'','DEVELOPMENT_TEAM'=>'','MARKETING_VERSION'=>'0.1.0','CURRENT_PROJECT_VERSION'=>ENV.fetch('BLOFY_IOS_BUILD_NUMBER','1')})
+ c.build_settings.merge!({'PRODUCT_BUNDLE_IDENTIFIER'=>'tv.blofy.player.ios','PRODUCT_NAME'=>'BlofyPlayer','SWIFT_VERSION'=>'5.0','IPHONEOS_DEPLOYMENT_TARGET'=>'16.0','TARGETED_DEVICE_FAMILY'=>'1,2','INFOPLIST_FILE'=>'BlofyPlayer/Info.plist','GENERATE_INFOPLIST_FILE'=>'NO','CODE_SIGNING_ALLOWED'=>'NO','CODE_SIGNING_REQUIRED'=>'NO','CODE_SIGN_IDENTITY'=>'','DEVELOPMENT_TEAM'=>'','MARKETING_VERSION'=>'0.2.0','CURRENT_PROJECT_VERSION'=>ENV.fetch('BLOFY_IOS_BUILD_NUMBER','1'),'FRAMEWORK_SEARCH_PATHS'=>['$(inherited)','$(PROJECT_DIR)/Vendor']})
 end
 project.save
 scheme=Xcodeproj::XCScheme.new; scheme.add_build_target(target); scheme.set_launch_target(target); scheme.save_as(project.path,'BlofyPlayer',true)
-puts 'Generated unsigned BlofyPlayer.xcodeproj'
+puts 'Generated BlofyPlayer.xcodeproj with VLCKit'
