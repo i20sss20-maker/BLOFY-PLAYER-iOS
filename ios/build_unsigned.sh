09ede405e1bb7cb27c727d6dacee7f45e379d59b
i20sss20-maker <i20sss20@gmail.com>
2026-09-08 07:06:59 +0300

    Package iOS premium polish as 0.3.1

--- ios/build_unsigned.sh
+++ ios/build_unsigned.sh
@@ -64,7 +64,7 @@ cp -R "$APP" build/package/Payload/
 rm -rf build/package/Payload/BlofyPlayer.app/_CodeSignature
 rm -f build/package/Payload/BlofyPlayer.app/embedded.mobileprovision
 
-NAME=BLOFY-PLAYER-iOS-0.3.0-unsigned.ipa
+NAME=BLOFY-PLAYER-iOS-0.3.1-unsigned.ipa
 (cd build/package && /usr/bin/ditto -c -k --keepParent Payload "../../dist/$NAME")
 shasum -a 256 "dist/$NAME" > dist/SHA256SUMS.txt
 file "$APP/BlofyPlayer" | tee dist/BINARY_INFO.txt
